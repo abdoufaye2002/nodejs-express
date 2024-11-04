4d123ce4b09e7c4c4a9037390008ff19a02f56b3
Abdou Faye <abdoufaye2021@gmail.com>
2024-11-04 03:19:25 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const placesRoutes = require("./routes/places-routes");
+const port = 3000;
+
+app.use("/api/place", placesRoutes);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500);
+  res.json({ message: error.message || "erreur non connu" });
+});
+
+app.listen(3000, () => {
+  console.log(`http://localhost:${port}`);
+});
---
+++ routes/places-routes.js
@@ -0,0 +1,44 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empiree",
+    description: "lorem upsum elit description",
+    location: {
+      lat: 40.9999,
+      long: -73.9822,
+    },
+    address: "avenue 209",
+    creator: "u1",
+  },
+];
+
+router.get("/:id", (req, res, next) => {
+  const placeId = req.params.id;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  if (!place) {
+    const error = new Error("ID NO TROUVER");
+    error.code = 404;
+    next(error);
+  }
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  if (!place) {
+    const error = new Error("ID NO TROUVER");
+    error.code = 404;
+    return next(error);
+  }
+  res.json({ place });
+});
+module.exports = router;
