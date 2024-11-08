985aedcb9bad4aae09a6d0a63b41869df016f34f
Abdou Faye <abdoufaye2021@gmail.com>
2024-11-08 02:35:25 +0000

    update

--- app.js
+++ app.js
@@ -2,10 +2,12 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const placesRoutes = require("./routes/places-routes");
+const usersRoutes = require("./routes/users-routes");
 const HttpError = require("./models/http-error");
 const port = 3000;
 app.use(bodyParser.json());
 app.use("/api/places", placesRoutes);
+app.use("/api/users", usersRoutes);
 
 app.use((req, res, next) => {
   const error = new HttpError("DONNEES NON TROUVER", 404);
---
+++ controllers/users-controller.js
@@ -0,0 +1,42 @@
+const { v4: uuidv4 } = require("uuid");
+const HttpError = require("../models/http-error");
+const DUMMY_USERS = [
+  {
+    id: "u1",
+    name: "abdou",
+    email: "test@test.com",
+    password: "tester",
+  },
+];
+const getUsers = (req, res, next) => {
+  res.json({ users: DUMMY_USERS });
+};
+
+const signup = (req, res, next) => {
+  const { name, email, password } = req.body;
+  const hasUser = DUMMY_USERS.find((u) => u.email === email);
+  if (hasUser) {
+    throw new HttpError("L'EMAIL EXISTE DEJA!", 422);
+  }
+  const createdUser = {
+    id: uuidv4(),
+    name, // name:name
+    email,
+    password,
+  };
+  DUMMY_USERS.push(createdUser);
+  res.status(201).json({ user: createdUser });
+};
+
+const login = (req, res, next) => {
+  const { email, password } = req.body;
+  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
+  if (!identifiedUser || identifiedUser.password !== password) {
+    throw new HttpError("PAS D'IDENTIFIANT TROUVE", 401);
+  }
+  res.json({ message: "CONNECTE" });
+};
+
+exports.getUsers = getUsers;
+exports.signup = signup;
+exports.login = login;
