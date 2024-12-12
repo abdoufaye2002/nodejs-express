d665edb1ef204a3050e5a0185c47c322b7ac7638
Abdou Faye <abdoufaye2021@gmail.com>
2024-12-12 02:13:14 +0000

    update

--- controllers/places-controller.js
+++ controllers/places-controller.js
@@ -124,8 +124,9 @@ const updatePlace = async (req, res, next) => {
   res.status(201).json({ place: place.toObject({ getters: true }) });
 };
 
-const deletePlace = (req, res, next) => {
+const deletePlace = async (req, res, next) => {
   const placeId = req.params.id;
+  let place;
   try {
   } catch (err) {
     const error = HttpError(
