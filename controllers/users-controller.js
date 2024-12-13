61c40036b45c8b9230d590518590c7a12c47f298
Abdou Faye <abdoufaye2021@gmail.com>
2024-12-13 22:46:47 +0000

    update

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -2,16 +2,27 @@ const { v4: uuidv4 } = require("uuid");
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
 const User = require("../models/users");
-const DUMMY_USERS = [
-  {
-    id: "u1",
-    name: "abdou",
-    email: "test@test.com",
-    password: "tester",
-  },
-];
-const getUsers = (req, res, next) => {
-  res.json({ users: DUMMY_USERS });
+// const DUMMY_USERS = [
+//   {
+//     id: "u1",
+//     name: "abdou",
+//     email: "test@test.com",
+//     password: "tester",
+//   },
+// ];
+const getUsers = async (req, res, next) => {
+  let users;
+  try {
+    users = await User.find({}, "-password");
+  } catch (err) {
+    const error = new HttpError(
+      "Recherche d'utilisateurs echouer,veuiller reessayer plus tard",
+      500
+    );
+    return next(error);
+  }
+  // Convertir chaque utilisateur en un objet JSON avec les getters
+  res.json({ Users: users.map((user) => user.toObject({ getters: true })) });
 };
 
 const signup = async (req, res, next) => {
@@ -58,11 +69,21 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
-const login = (req, res, next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
-  const identifiedUser = DUMMY_USERS.find((u) => u.email === email);
-  if (!identifiedUser || identifiedUser.password !== password) {
-    throw new HttpError("PAS D'IDENTIFIANT TROUVE", 401);
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "La connexion a echoue,veuillez reessayer plus tard",
+      500
+    );
+    return next(error);
+  }
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError("Donnee de connexion invalide", 401);
+    return next(error);
   }
   res.json({ message: "CONNECTE" });
 };
@@ -70,4 +91,3 @@ const login = (req, res, next) => {
 exports.getUsers = getUsers;
 exports.signup = signup;
 exports.login = login;
-//login
