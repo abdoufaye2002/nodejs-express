74e86cc4d09db9f136a028b61c622ea2c61492e7
Abdou Faye <abdoufaye2021@gmail.com>
2024-12-13 04:59:58 +0000

    update

--- models/users.js
+++ models/users.js
@@ -5,5 +5,8 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  password: { type: String, required: true, minlength: 6 },
+  image: { type: String, required: true },
 });
+
+module.exports = mongoose.model("User", userSchema);
