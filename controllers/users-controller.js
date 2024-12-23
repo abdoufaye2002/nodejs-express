1c92b5c76711d42f8413393d15eff811822f7784
Abdou Faye <abdoufaye2021@gmail.com>
2024-12-23 01:13:31 +0000

    update

--- controllers/users-controller.js
+++ controllers/users-controller.js
@@ -1,4 +1,4 @@
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 const HttpError = require("../models/http-error");
 const { validationResult } = require("express-validator");
 const User = require("../models/users");
